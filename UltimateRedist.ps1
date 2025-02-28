45c58ef83e71009ce908971ea6d933acaebf3423
darthdemono <thedarkterra569@gmail.com>
2025-03-01 05:37:43 +0600

    Removed Useless Function

--- UltimateRedist.ps1
+++ UltimateRedist.ps1
@@ -20,27 +20,25 @@ if ([Environment]::Is64BitOperatingSystem) {
     $ssceUrl = "https://download.microsoft.com/download/f/f/d/ffdf76e3-9e55-41da-a750-1798b971936c/ENU/SSCERuntime_x64-ENU.exe"
     $uiXaml = Join-Path $depFolder "x64\Microsoft.UI.Xaml.2.8_8.2501.31001.0_x64.appx"
     $vcLibs = Join-Path $depFolder "x64\Microsoft.VCLibs.140.00.UWPDesktop_14.0.33728.0_x64.appx"
-} else {
+}
+else {
     $ssceUrl = "https://download.microsoft.com/download/f/f/d/ffdf76e3-9e55-41da-a750-1798b971936c/ENU/SSCERuntime_x32-ENU.exe"
     $uiXaml = Join-Path $depFolder "x86\Microsoft.UI.Xaml.2.8_8.2501.31001.0_x86.appx"
     $vcLibs = Join-Path $depFolder "x86\Microsoft.VCLibs.140.00.UWPDesktop_14.0.33728.0_x86.appx"
 }
 
-function Try-AddAppxPackage {
-    param (
-        [Parameter(Mandatory=$true)]
-        [string]$PackagePath
-    )
-    try {
-        Add-AppxPackage -Path $PackagePath -ErrorAction Stop
-    } catch {
-        if ($_.Exception.Message -match "0x80073D02") {
-            Write-Host "Package '$PackagePath' already installed, skipping."
-        } else {
-            throw $_
-        }
+try {
+    Add-AppxPackage -Path $PackagePath -ErrorAction Stop
+}
+catch {
+    if ($_.Exception.Message -match "0x80073D02") {
+        Write-Host "Package '$PackagePath' already installed, skipping."
+    }
+    else {
+        throw $_
     }
 }
+
 
 Write-Host "Installing Microsoft.UI.Xaml dependency..."
 Try-AddAppxPackage -PackagePath $uiXaml
@@ -55,10 +53,12 @@ Invoke-WebRequest -Uri $msixUrl -OutFile $msixPath
 
 try {
     Add-AppxPackage -Path $msixPath -ErrorAction Stop
-} catch {
+}
+catch {
     if ($_.Exception.Message -match "0x80073D02") {
         Write-Host "Microsoft.DesktopAppInstaller already installed, skipping."
-    } else {
+    }
+    else {
         throw $_
     }
 }
@@ -90,7 +90,7 @@ winget install -e --id Oracle.JavaRuntimeEnvironment --source winget --silent
 
 # Visual J# Redistributable
 Write-Host "Downloading and installing Visual J# Redistributable..."
-$vjUrl  = "https://download.microsoft.com/download/9/a/0/9a01eb1e-fe80-41af-a3f8-ea41220918f7/vjredist.exe"
+$vjUrl = "https://download.microsoft.com/download/9/a/0/9a01eb1e-fe80-41af-a3f8-ea41220918f7/vjredist.exe"
 $vjPath = "$env:TEMP\vjredist.exe"
 Invoke-WebRequest -Uri $vjUrl -OutFile $vjPath
 Start-Process -FilePath $vjPath -Wait
@@ -103,8 +103,8 @@ Start-Process -FilePath $sscePath -Wait
 
 # OpenAL Installer
 Write-Host "Downloading and installing OpenAL..."
-$oalUrl    = "https://www.openal.org/downloads/oalinst.zip"
-$oalZip    = "$env:TEMP\oalinst.zip"
+$oalUrl = "https://www.openal.org/downloads/oalinst.zip"
+$oalZip = "$env:TEMP\oalinst.zip"
 $oalFolder = "$env:TEMP\oalinst"
 Invoke-WebRequest -Uri $oalUrl -OutFile $oalZip
 Expand-Archive -Path $oalZip -DestinationPath $oalFolder -Force
